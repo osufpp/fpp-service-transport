2542ec91bfb3d8785f7a89dd7622e32596b0593e
tyscorp <tyscorp@gmail.com>
2016-04-01 13:38:10 -0500

    Throw error on missing credentials

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const amqp = require('amqplib');
 const config = require('config');
 const logger = require('@osufpp/logger');
@@ -5,14 +6,22 @@ const Promise = require('bluebird');
 
 Promise.promisifyAll(amqp);
 
-const connection = amqp.connect(`amqp://${config.rabbitmq.url}${config.rabbitmq.vhost}`);
+const getConnection = _.once(function getConnection() {
+    if (!config.rabbitmq || !config.rabbitmq.vhost) {
+        throw new Error('No RabbitMQ credentials provided');
+    }
 
-connection.catch(function (e) {
-    logger.error(e);
+    const connection = amqp.connect(`amqp://${config.rabbitmq.url}${config.rabbitmq.vhost}`);
+
+    connection.catch(function (e) {
+        logger.error(e);
+    });
+
+    return connection;
 });
 
 module.exports = function createChannel() {
-    return Promise.resolve(connection)
+    return getConnection()
         .call('createChannel')
         .disposer(function (ch) {
             try {
